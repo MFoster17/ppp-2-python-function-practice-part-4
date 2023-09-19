6af053783c559055eaa5deecc54169cc970f82bf
MFoster17 <ausslanderofficial@gmail.com>
2023-09-19 15:40:01 -0600

    fixed my pascal answer

--- main.py
+++ main.py
@@ -44,18 +44,34 @@ print(num_within(10,2,5))
 
 # 5. pascal()
 
+triangle = [[1],[1,1]]
 def pascal(n):
-  rows = []
-  rows.append([1])
 
-  for i in range(1, n):
-    row = []
-    for j in range(i + 1):
-      if j == 0 or j == i:
-        row.append(1)
-      else:
-        row.append(rows[i - 1][j - 1] + rows[i - 1][j])
-    rows.append(row)
+  if n < 1:
+    print("invalid number of rows")
+  elif n == 1:
+    print(triangle[0])
+  else:
+    row_number = 2
 
-  for row in rows:
-    print(row)
+    while len(triangle) < n:
+      row = []
+      row_prev = triangle[row_number - 1]
+
+      for i in range(length):
+
+        if i == 0:
+          row.append(1)
+        elif i > 0 and i < length-1:
+          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+
+        else:
+          row.append(1)
+      triangle.append(row)
+      row_number += 1
+      
+    for row in triangle:
+      print(row)
+
+pascal(2)
+pascal(5)
